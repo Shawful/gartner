720e32f6bc469fec484583be3643b6f8b9baf527
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-01-09 08:37:03 -0600

    modal layed out

--- controller.js
+++ controller.js
@@ -1,8 +1,25 @@
-var app = angular.module('buttonABTest', ['ngCookies', 'ngAnimate']);
+var app = angular.module('buttonABTest', ['ngCookies', 'ngAnimate', 'ui.bootstrap']);
 
-app.controller('myCtrl' ,['$scope', '$rootScope', '$cookies', 
-	function($scope, $rootScope, $cookies) {
+app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 
+	function ($scope, $uibModalInstance) {
+
+		$scope.submit = function () {
+			console.log('submit clicked');
+			$uibModalInstance.close();
+		};
+
+		$scope.cancel = function () {
+			console.log('cancel clicked');
+			$uibModalInstance.dismiss('cancel');
+		};
+	}
+]);
+
+app.controller('myCtrl' ,['$scope', '$rootScope', '$cookies', '$uibModal', 
+	function($scope, $rootScope, $cookies, $uibModal) {
 		
+		var states = ['Alabama', 'Texas'];
+		var animationsEnabled = true;
 		var click_info = {
 			'session_id': 'blank_initially'
 		};
@@ -10,6 +27,25 @@ app.controller('myCtrl' ,['$scope', '$rootScope', '$cookies',
 		$scope.buttonClicked = function () {
 			click_info.session_id = Date();
 			console.log(click_info);
+			$scope.openModal();
+		};
+
+		$scope.openModal = function() {
+			
+		    var modalInstance = $uibModal.open({
+		      animation: animationsEnabled,
+		      templateUrl: 'myModalContent.html',
+		      controller: 'ModalInstanceCtrl',
+		    });
+
+		    modalInstance.result.then(function () {
+		      // Data Submitted
+		    }, function () {
+		      // Modal dismissed
+		      console.log('Modal dismissed at: ' + new Date());
+		    });
+
+		    
 		};
 	}
 ]);
